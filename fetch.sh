13af242159a3d9db99074ad24cc3f966fb906338
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-11-09 03:18:24 +0100

    Create fetch.sh

---
+++ fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LINUX_PREVIEW_LATEST=`curl -s 'https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/versions.json' | jq -r '.linux.preview'`
+LINUX_PREVIEW="https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/linux_preview/$LINUX_PREVIEW_LATEST.json"
+
+DOWNLOAD_URL=`curl -s $LINUX_PREVIEW | jq -r '.download_url'`
+echo "Minecraft v$LINUX_PREVIEW_LATEST - $DOWNLOAD_URL"
+
+wget $DOWNLOAD_URL -q -O bds.zip
+unzip bds.zip -d bds
+echo '{ "generate_documentation": true }' > 'bds/test_config.json'
+cd bds && ./bedrock_server
+cp -r docs ../docs
